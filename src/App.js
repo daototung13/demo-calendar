a9f788e75b81fb8d1a718f65ce7a02fa111b6116
Tung Dao To <tung.dao1@ntq-solution.com.vn>
2019-06-06 18:07:09 +0700

    'test'

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
+import BigCalendar from 'react-big-calendar'
+import moment from 'moment'
 import './App.css';
+import events from './events'
+let allViews = Object.keys(BigCalendar.Views).map(k => BigCalendar.Views[k])
+const localizer = BigCalendar.momentLocalizer(moment);
+
+const MyCalendar = (props) => {
+  return(
+    <div className='calendar'>
+      <BigCalendar
+        events={events}
+        views={allViews}
+        step={60}
+        showMultiDayTimes
+        max={2015, 17, 1}
+        defaultDate={new Date(2015, 3, 1)}
+        components={{
+          timeSlotWrapper: ColoredDateCellWrapper,
+        }}
+        localizer={localizer}
+      />
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MyCalendar />
     </div>
   );
 }
+
 
 export default App;
